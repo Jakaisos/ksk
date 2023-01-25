b835065035cb0e894df461cfb4191d19206a2d57
Jakaisos <123408501+Jakaisos@users.noreply.github.com>
2023-01-25 21:05:28 +0300

    Add files via upload

---
+++ jmthon.py
@@ -0,0 +1,141 @@
+import asyncio
+import os
+from telethon import TelegramClient, events
+from bothseesion import bothseesion
+from logger import logging
+
+# by:e  ~ t.me/R0R77
+
+import asyncio
+import random
+
+import requests
+import telethon
+from telethon.sync import functions
+from user_agent import generate_user_agent
+
+from sbb_b import sbb_b
+
+
+API_ID = int(os.environ.get("APP_ID", 6))
+API_HASH = os.environ.get("API_HASH") or None
+
+LOGS = logging.getLogger("سورس جمثون")
+
+sessions = [
+    'session_user_1',
+    'session_user_2',
+    'session_user_3',
+]
+
+
+session = bothseesion(SESSION, LOGS)
+client = TelegramClient(session, API_ID, API_HASH)
+
+
+client.start()
+
+
+a = "qwertyuiopassdfghjklzxcvbnm"
+b = "1234567890"
+e = "qwertyuiopassdfghjklzxcvbnm1234567890"
+
+trys, trys2 = [0], [0]
+isclaim = ["off"]
+isauto = ["off"]
+
+
+def check_user(username):
+    url = "https://t.me/" + str(username)
+    headers = {
+        "User-Agent": generate_user_agent(),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7",
+    }
+
+    response = requests.get(url, headers=headers)
+    if (
+        response.text.find(
+            'If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"'
+        )
+        >= 0
+    ):
+        return True
+    else:
+        return False
+
+@client.on(events.NewMessage(outgoing=True, pattern=".تثبيت (.*)"))
+async def _(event):
+    msg = event.text.split()
+    try:
+        ch = str(msg[2])
+        await event.edit(f"حسناً سيتم بدء التثبيت في**-  @{ch} .**")
+    except:
+        try:
+            ch = await sbb_b(
+                functions.channels.CreateChannelRequest(
+                    title="JMTHON HUNTER - صيد جمثون",
+                    about="This channel to hunt username by - @jmthon ",
+                )
+            )
+            ch = ch.updates[1].channel_id
+            await event.edit(f"**- تم بنجاح بدأ التثبيت**")
+        except Exception as e:
+            await sbb_b.send_message(
+                event.chat_id, f"خطأ في انشاء القناة , الخطأ : {str(e)}"
+            )
+    isauto.clear()
+    isauto.append("on")
+    username = str(msg[1])
+
+    for i in range(1000000000000):
+        isav = check_user(username)
+        if isav == True:
+            try:
+                await sbb_b(
+                    functions.channels.UpdateUsernameRequest(
+                        channel=ch, username=username
+                    )
+                )
+                await event.client.send_message(
+                    event.chat_id,
+                    f"- Done : @{username} !\n- By : @R0R77 - @JMTHON !\n- Hunting Log {trys2[0]}",
+                )
+                break
+            except telethon.errors.rpcerrorlist.UsernameInvalidError:
+                await event.client.send_message(
+                    event.chat_id, f"المعرف **-  @{username} غير صالح . **"
+                )
+                break
+            except telethon.errors.FloodError as e:
+                await sbb_b.send_message(
+                    event.chat_id, f"للاسف تبندت , مدة الباند ({e.seconds}) ثانية ."
+                )
+                break
+            except Exception as eee:
+                await sbb_b.send_message(
+                    event.chat_id,
+                    f"""خطأ مع {username} , الخطأ :{str(eee)}""",
+                )
+                break
+        else:
+            pass
+        trys2[0] += 1
+
+        await asyncio.sleep(1.3)
+    isclaim.clear()
+    isclaim.append("off")
+    await sbb_b.send_message(event.chat_id, "**- تم الانتهاء من التثبيت بنجاح**")
+
+@client.on(events.NewMessage(outgoing=True, pattern=".حالة التثبيت"))
+async def _(event):
+    if "on" in isauto:
+        await event.edit(f"**- التثبيت وصل لـ({trys2[0]}) من المحاولات**")
+    elif "off" in isauto:
+        await event.edit("**- التثبيت بالاصل لا يعمل .**")
+    else:
+        await event.edit("-لقد حدث خطأ ما وتوقف الامر لديك")
+
+print("تم بنجاح تشغيل سورس النشر المميز")
+client.run_until_disconnected()
